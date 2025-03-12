1241d2bc22cfd24a987c35833b8a32ab1d7034b5
Kuica <jpedrovasconcelosfaria@gmail.com>
2025-03-12 13:53:58 +0000

    classe atualizada

--- Bus.java
+++ Bus.java
@@ -1,8 +1,3 @@
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Bus {
     private float temperatura;
     private int capacidade;
@@ -14,9 +9,49 @@ public class Bus {
         this.lotacao = lotacao;
     }
 
-    public float percentagem_lotacao () {
-        return (float) lotacao * 100 / capacidade;
+//SETTERS-------
+    public void setTemperatura(float temperatura) {
+        if(temperatura < -20 || temperatura > 50) {
+            throw new IllegalArgumentException("Temperatura fora dos valores expectáveis");
+        } 
+        this.temperatura = temperatura;
     }
 
+    public void setCapacidade(int capacidade) {
+        if(capacidade < 0) {
+            throw new IllegalArgumentException("Capacidade não pode ser menor que 0");
+        }
+        this.capacidade = capacidade;
+    }
+
+    public void setLotacao(int lotacao) {
+        if (lotacao > capacidade) {
+            throw new IllegalArgumentException("Lotação não pode exceder a capacidade.");
+        }
+        this.lotacao = lotacao;
+    }
+//--------------
+
+//GETTERS-------
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public int getLotacao() {
+        return lotacao;
+    }
+//--------------
+
+    public float percentagem_lotacao() {
+        if (capacidade == 0) {
+            throw new ArithmeticException("Capacidade não pode ser zero.");
+        }
+        return (float) lotacao * 100 / capacidade;
+    }
+    
     
 }
