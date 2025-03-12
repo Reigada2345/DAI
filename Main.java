32b2d682fab05a2f5947c6fe735db129066f1873
Kuica <jpedrovasconcelosfaria@gmail.com>
2025-03-12 14:42:19 +0000

    mais4

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Bus bus1 = new Bus(0, 80, 0);
-        Sensores sensor = new Sensores(bus1);
-        sensor.atualizarSensores();
-        sensor.exibirDados();
+        Bus bus2 = new Bus(0, 120, 0);
+        Bus bus3 = new Bus(0, 40, 0);
+        Sensores sensor1 = new Sensores(bus1);
+        Sensores sensor2 = new Sensores(bus2);
+        Sensores sensor3 = new Sensores(bus3);
+        sensor1.atualizarSensores();
+        sensor1.exibirDados();
+        sensor2.atualizarSensores();
+        sensor2.exibirDados();
+        sensor3.atualizarSensores();
+        sensor3.exibirDados();
     }
 }
