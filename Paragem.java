ff8731f0778c05f3cfef6e20f042bf3ca0048f03
KEARNEY2005 <a108284@uminho.pt>
2025-04-29 12:54:17 +0100

    o administrador cria uma paragem

--- Paragem.java
+++ Paragem.java
@@ -1,13 +1,31 @@
 public class Paragem {
+    private String nome;
+    private String localizacao;
     private boolean variasRotas;
     private int lotacao;
+    private boolean ativa = true;
 
     public Paragem (boolean variasRotas, int lotacao) {
+        this.nome = nome;
+        this.localizacao = localizacao;
         this.lotacao = lotacao;
         this.variasRotas = variasRotas;
+        this.ativa = true;
+    }
+
+    public static Paragem criarParagem(boolean variasRotas, int lotacao) {
+        return new Paragem(nome, localizacao, variasRotas, lotacao);
     }
 
 //GETTERS------
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
     public boolean getVariasRotas() {
         return variasRotas;
     }
@@ -17,6 +35,14 @@ public class Paragem {
     }
 //-------------
 //SETTERS------
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
     public void setVariasRotas(boolean variasRotas) {
         this.variasRotas = variasRotas;
     }
@@ -26,4 +52,11 @@ public class Paragem {
     }
 //-------------
 
+    public void desativarParagem() {
+        this.ativa = false; // Marca a paragem como desativada
+    }
+
+    public boolean isAtiva() {
+        return ativa; // Retorna o estado da paragem
+    }
 }
