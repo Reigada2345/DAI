efd2b3767976dd2d657e8af0c4822bec81efbb04
KEARNEY2005 <a108284@uminho.pt>
2025-04-29 18:44:10 +0100

    import cliente no admin

--- AdministratorProject/src/models/Administrador.java
+++ AdministratorProject/src/models/Administrador.java
@@ -1,5 +1,5 @@
 package models;
-
+import Cliente;
 public class Administrador extends Cliente {
 
     public Administrador(String nome) {
--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,3 @@
-package models;
 
 public class Cliente {
     private String email;
