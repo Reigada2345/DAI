fa466feea6265c630889d040148ac6d9563f77d0
Reigada2345 <carolinabreigada@gmail.com>
2025-04-30 02:55:06 +0100

    a tentar resolver os erros, nao consegui, vou dormir

---
+++ AdministratorProject/src/models/Capacidade.java
@@ -0,0 +1,24 @@
+package models;
+
+public class Capacidade {
+    private int capacidadeMaxima;
+
+    public Capacidade(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public void setCapacidadeMaxima(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    @Override
+    public String toString() {
+        return "Capacidade{" +
+                "capacidadeMaxima=" + capacidadeMaxima +
+                '}';
+    }
+}
