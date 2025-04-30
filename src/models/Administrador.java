fa735262e10308e18abad2994c3144e63ce899cd
JoaoLeite04 <joaosleite10@gmail.com>
2025-04-30 23:48:06 +0100

    adm sem nome

--- src/models/Administrador.java
+++ src/models/Administrador.java
@@ -1,22 +1,22 @@
 package models;
 
 public class Administrador extends Cliente {
-    private String nome;
 
-public String getNome() { // Método para acessar o valor de nome
-        return nome;
+    public Administrador(String nomeProprio, String apelido, String contacto, 
+                       boolean utilizadorPrioritario, String email, String password) {
+        super(nomeProprio, apelido, contacto, utilizadorPrioritario, email, password);
     }
-    public Administrador(String nome, String nomeProprio, String apelido, String contacto, 
-                   boolean utilizadorPrioritario, String email, String password) {
-    super(nomeProprio, apelido, contacto, utilizadorPrioritario, email, password);
-    this.nome = nome;
-}
+
+    public String getNome() {
+        return getNomeProprio() + " " + getApelido();  
+    }
 
     public Paragem criarParagem(String nome, String localizacao, boolean variasRotas, int lotacao) {
         return new Paragem(nome, localizacao, variasRotas, lotacao);
     }
 
-    public void editarParagem(Paragem paragem, String novoNome, String novaLocalizacao, boolean novasRotas, int novaLotacao) {
+    public void editarParagem(Paragem paragem, String novoNome, String novaLocalizacao, 
+                            boolean novasRotas, int novaLotacao) {
         paragem.editarParagem(novoNome, novaLocalizacao, novasRotas, novaLotacao);
     }
 
@@ -28,11 +28,13 @@ public String getNome() { // Método para acessar o valor de nome
         return new Autocarro(numero, capacidade, rota);
     }
 
-    public void editarAutocarro(Autocarro autocarro, String novoNumero, int novaCapacidade, String novaRota) {
+    public void editarAutocarro(Autocarro autocarro, String novoNumero, 
+                              int novaCapacidade, String novaRota) {
         autocarro.editarAutocarro(novoNumero, novaCapacidade, novaRota);
     }
 
-    public void atualizarPassageiro(Passageiro passageiro, String novoNome, int novaIdade, String novoBilhete) {
+    public void atualizarPassageiro(Passageiro passageiro, String novoNome, 
+                                  int novaIdade, String novoBilhete) {
         passageiro.atualizarInformacoes(novoNome, novaIdade, novoBilhete);
     }
 }
